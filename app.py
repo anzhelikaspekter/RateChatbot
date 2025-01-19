52991faefa1c342b823d87aa601231fd4ff31b30
Angelika Specter <angelikaspecter@gmail.com>
2025-01-19 17:29:17 +0200

    UPD txt

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/rating', methods=['POST'])
+def calculate_rating():
+    data = request.json
+    total_rating = int(data.get('total_rating', 0))
+    rating_count = int(data.get('rating_count', 0))
+
+    if rating_count > 0:
+        average_rating = round(total_rating / rating_count, 2)
+    else:
+        average_rating = 0.0
+
+    return jsonify({"average_rating": average_rating})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
